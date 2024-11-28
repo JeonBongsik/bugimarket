94f83df34c98de9338f82890aebb661d9f0b76b6
JeonBongsik <wjsdudtlr86@gmail.com>
2024-11-29 01:23:28 +0900

    test

---
+++ src/main/java/com/bugimarket/BugimarketApplication.java
@@ -0,0 +1,13 @@
+package com.bugimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugimarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugimarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bugimarket/BugimarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugimarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugimarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
